88c0a4f7de80e9a81ac9f9a452fad795237dbbb0
Andrei Gaevskii <andrej.gaevskij@gmail.com>
2022-03-09 22:49:18 +0300

    Add vial.json for lotus58

---
+++ keyboards/lotus58/rules.mk
@@ -0,0 +1,21 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+SPLIT_KEYBOARD = yes
+NKRO_ENABLE = yes
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
+ENCODER_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+
+
